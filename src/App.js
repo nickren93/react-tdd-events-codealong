2a3b4ff87aaeb55a471e9b23223620e17e523b30
Sitong <nickren93@gmail.com>
2025-03-17 18:09:20 -0400

    complete code along

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+//add import use state:
+import { useState } from "react";
+
 function App() {
+  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+
+  function togglePepperoni(e) {
+    setPepperoniIsChecked(e.target.checked);
+  }
+
   return (
-    <div></div>
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        /*
+        checked={false}
+        aria-checked={false}
+        */
+        //update the above two lines of code to following:
+        checked={pepperoniIsChecked}
+        aria-checked={pepperoniIsChecked}
+        onChange={togglePepperoni}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniIsChecked ? <li>Pepperoni</li> : null}
+      </ul>
+    </div>
   )
 }
 
